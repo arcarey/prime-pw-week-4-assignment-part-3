b0d621f48894c516ebc2c468ea09f37a89a733f8
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-10 22:00:19 -0500

    added clarifying notes about the function addItems

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,6 +3,7 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket = [];
+// I decided to make this function with 2 parameters so it could affect any array rather than just our basket
 function addItem(array, item){
     if (Array.isArray(array) == false || typeof (item) !== 'string'){  //if the parameters array isn't and array and item isn't a string, return an error
         return 'Error: addItem() parameter 1 must be an array and parameter 2 must be a string';
@@ -11,7 +12,10 @@ function addItem(array, item){
         return true;
     }
 }
+
+//testing wrong and right data types with this new function
 console.log(addItem(1, 'bread'));
 console.log(addItem(basket, 1));
 console.log(addItem(basket, 'Bread'));
 console.log(basket);
+
