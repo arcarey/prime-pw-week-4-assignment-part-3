5b5730bf1b3aafa14660050b81240434f5ef7da5
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-10 21:40:17 -0500

    started cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,11 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+function addItem(array, item){
+    if (array.isArray === false || item.isString === false){
+        return 'Error: addItem parameter 1 must be an array and parameter 2 must be a string'
+    }         //if the parameters array isn't and array and item isn't a string, return an error
+    
+}
