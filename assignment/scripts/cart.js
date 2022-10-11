19f75ffe029a557027187d2d7e62e7bb893d2a8f
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-10 22:19:16 -0500

    finished questions. Needs better test formatting and finish stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,11 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+// create an empty basket
 let basket = [];
-// I decided to make this function with 2 parameters so it could affect any array rather than just our basket
+
+// create a function that adds items to the basket
+// I decided to make this function with 2 parameters so it could add a string to any array rather than just our basket
 function addItem(array, item){
     if (Array.isArray(array) == false || typeof (item) !== 'string'){  //if the parameters array isn't and array and item isn't a string, return an error
         return 'Error: addItem() parameter 1 must be an array and parameter 2 must be a string';
@@ -14,8 +17,33 @@ function addItem(array, item){
 }
 
 //testing wrong and right data types with this new function
-console.log(addItem(1, 'bread'));
+console.log(addItem(1, 'bread')); 
 console.log(addItem(basket, 1));
 console.log(addItem(basket, 'Bread'));
 console.log(basket);
+console.log(addItem(basket, 'Peaunut Butter'));
+console.log(basket);
 
+// create a function that logs each item in the basket
+// this also takes the array as an argument 
+function listItems (array){
+    for ( const x of array) {
+        console.log(x);
+    }
+}
+
+//test listItems
+listItems(basket);
+
+// create a function that empties an array
+function empty (array) {
+    array = [];
+    return array;
+}
+
+// test empty
+console.log(empty(basket));
+
+// for next login:
+// add better log text for testing
+// do stretch goals
